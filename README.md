8928db7cf296183bf69f8463f68789f90d5feb33
Hackbright Student <info@hackbrightacademy.com>
2015-03-17 13:56:32 -0700

    ran linter on js file

--- README.md
+++ README.md
@@ -7,4 +7,26 @@
 
  Driven to solve a longstanding problem in her community, Daria created LGBT MD to connect Lesbian, Gay, Bisexual, and Transgender people to well-trained medical providers. Discover providers near you by searching by both location and type. Bookmark your favorites and text yourself their contact info. Contribute reviews and new providers to help others find the care they need.<br>
 
-[Screencast Demo of LGBT MD](http://youtu.be/j_h-nyEkpcI)
+[Screencast Demo of LGBT MD](http://youtu.be/j_h-nyEkpcI)<br><br>
+
+The MIT License (MIT)
+
+Copyright (c) <year> <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- static/js/map2.js
+++ static/js/map2.js
@@ -17,251 +17,220 @@ var showTherapy = document.getElementById('filter-therapy');
 var CENTER = L.latLng(37.8, -122.4);
 var RADIUS = 20 * 1609.34;
 
-
 //waits for all DOM elements to load
-$(document).ready(function(){
-
-
-    pinLayer.on('mouseover', function(e) {
-        e.layer.openPopup();
-    });
-
+$(document).ready(function() {
+  pinLayer.on('mouseover', function(e) {
+    e.layer.openPopup();
+  });
 
     //events listener on "show all" changes the value of it's class name and calls mapSearch
-    showIns.onclick = function(e) {
-        showAll.className = '';
-        this.className = 'active';
-        mapSearch();
+  showIns.onclick = function(e) {
+    showAll.className = '';
+    this.className = 'active';
+    mapSearch();
         //why is this this way?
-        return false;
-    };
+    return false;
+  };
 
     //events listener on "show all" changes the value of it's class name and calls mapSearch
-    showAll.onclick = function() {
-        showIns.className = '';
-        this.className = 'active';
-        showTrans.className='';
-        showTherapy.className='';
-        mapSearch();
-        return false;
-    };
+  showAll.onclick = function() {
+    showIns.className = '';
+    this.className = 'active';
+    showTrans.className = '';
+    showTherapy.className = '';
+    mapSearch();
+    return false;
+  };
 
-    showTrans.onclick = function() {
-        showAll.className = '';
-        this.className = 'active';
-        mapSearch();
-        return false;
-    };
+  showTrans.onclick = function() {
+    showAll.className = '';
+    this.className = 'active';
+    mapSearch();
+    return false;
+  };
 
-    showTherapy.onclick = function(e) {
-        showAll.className = '';
-        this.className = 'active';
-        mapSearch();
+  showTherapy.onclick = function(e) {
+    showAll.className = '';
+    this.className = 'active';
+    mapSearch();
         //why is this this way?
-        return false;
-    };
-
-
+    return false;
+  };
 
     //when submit is clicked, grab value in address field and send to be geocoded
-    $("#radiussubmit").click(function(evt){
-        evt.preventDefault();
-        getGeocode($("#address").val());
+  $("#radiussubmit").click(function(evt) {
+      evt.preventDefault();
+      getGeocode($("#address").val());
     });
 
     //when pin is clicked, grab values from its GeoJSON object, show them, and link them to /ratings
-    pinLayer.on('click', function(e) {
-        map.panTo(e.layer.getLatLng());
-        e.layer.openPopup();
-        var name = e.layer.feature.properties.title;
-        var address = e.layer.feature.properties.Address;
-        var phone = e.layer.feature.properties.phone;
-        var id = e.layer.feature.properties.idd;
-        var cert= e.layer.feature.properties.cert;
-        $("#sendtext").html('<form class="form-inline" id="textmess"><h4>Get a text with this doctors information </h4><div class="form-group"><input id="phone" type="text" class="form-control" placeholder="Your phone number" required/></div><button type="submit" id="submitphone" name="reviewsubmit" class="btn btn-default">Text Me</button></form><br></div><button id="faves" name="faves" class="btn btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Add/Remove</button><br><br>');
-        $("#basic-info").html('<div class="list-group"><a href="#" class="list-group-item active">'+ name + ' '+ cert +'</a><a href="#" class="list-group-item">'+phone+'</a><a href="#" class="list-group-item">'+address+'</a>');
+  pinLayer.on('click', function(e) {
+    map.panTo(e.layer.getLatLng());
+    e.layer.openPopup();
+    var name = e.layer.feature.properties.title;
+    var address = e.layer.feature.properties.Address;
+    var phone = e.layer.feature.properties.phone;
+    var id = e.layer.feature.properties.idd;
+    var cert = e.layer.feature.properties.cert;
+    $("#sendtext").html('<form class="form-inline" id="textmess"><h4>Get a text with this doctors information </h4><div class="form-group"><input id="phone" type="text" class="form-control" placeholder="Your phone number" required/></div><button type="submit" id="submitphone" name="reviewsubmit" class="btn btn-default">Text Me</button></form><br></div><button id="faves" name="faves" class="btn btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Add/Remove</button><br><br>');
+    $("#basic-info").html('<div class="list-group"><a href="#" class="list-group-item active">'+ name + ' '+ cert +'</a><a href="#" class="list-group-item">'+phone+'</a><a href="#" class="list-group-item">'+address+'</a>');
 
+    var url = "/ratings/" + id;
 
+    $("#faves").on('click', function(evt) {
+      evt.preventDefault();
+      console.log("in faves");
+      $.post("/addfave", {"data" : id}, function(result) {
+        console.log("yes, it worked");
+      });
+      $.post("/returnfaves", function(result) {
+          console.log(result.result);
+          $("#modal2text").empty();
 
-        var url = "/ratings/" + id;
-
-        $("#faves").on('click', function(evt){
-            evt.preventDefault();
-            console.log("in faves");
-            $.post("/addfave", {"data" : id}, function(result){
-                console.log("yes, it worked");
-            });
-            $.post("/returnfaves", function(result){
-                console.log(result.result);
-                $("#modal2text").empty();
-
-                // $.each(result.result, function(key, value){
-                //     console.log(key + value);
-                //     $("modal2text").text(key + value);
-
-                // });
-                // $("modal2text").append(($.each(result.result), function(key, value) {
-
-                // }));
-            for (var key in result.result) {
-                if (result.result.hasOwnProperty(key)) {
-                    console.log(key + " -> " + result.result[key]);
-                    $("#modal2text").append('<div class="list-group"><a href="#" class="list-group-item active">'+key+'</a><a href="#" class="list-group-item">'+result.result[key][0]+'</a><a href="#" class="list-group-item">'+result.result[key][1]+'</a><a href="#" class="list-group-item">'+result.result[key][2]+'</a></div>');
-                }
+          for (var key in result.result) {
+            if (result.result.hasOwnProperty(key)) {
+              console.log(key + " -> " + result.result[key]);
+              $("#modal2text").append('<div class="list-group"><a href="#" class="list-group-item active">'+key+'</a><a href="#" class="list-group-item">'+result.result[key][0]+'</a><a href="#" class="list-group-item">'+result.result[key][1]+'</a><a href="#" class="list-group-item">'+result.result[key][2]+'</a></div>');
             }
-
-
-
-        });
-        });
-
-        $("#provider-detail").load(url, function(){
-            reviewEvent(id);
+          }
 
         });
+    });
+
+    $("#provider-detail").load(url, function() {
+      reviewEvent(id);
 
     });
+
+  });
 
 // FIXME rename!!! grabs, serializes contents of review form and posts it to /addreview route
-function reviewEvent(id) {
-    
+  function reviewEvent(id) {
     $("#textmess").on('submit', function(evt) {
-        evt.preventDefault();
-        $("submitphone").attr("disabled",true);
-        var phone = $("#phone").val();
-        console.log("in event listender");
-        console.log(phone);
-        sendInfo(id, phone);
-        $("#textmess").hide();
-        
+      evt.preventDefault();
+      $("submitphone").attr("disabled", true);
+      var phone = $("#phone").val();
+      console.log("in event listender");
+      console.log(phone);
+      sendInfo(id, phone);
+      $("#textmess").hide();     
     });
 
-
-    $("#reviewform").on('submit', function(evt){
-        evt.preventDefault();
+    $("#reviewform").on('submit', function(evt) {
+      evt.preventDefault();
             //gets contents of submit review form
-            var contents = $(this).serializeArray();
+      var contents = $(this).serializeArray();
             //appends doctor id
-            contents.push({"name": "doctor_id", "value": id});
-            var url = "/addreview";
+      contents.push({"name": "doctor_id", "value": id});
+      var url = "/addreview";
             //sends this info in post to the add review route
-            $.post(url, contents, function (result) {
+      $.post(url, contents, function(result) {
                // Your Flask has addded that review
                // return the list of comments for the doctor
 
                // FIXME here we're loading another url in an attempt to refresh review but only works once
-               var url2 = "/ratings/" + id;
+        var url2 = "/ratings/" + id;
 
-               $("#provider-detail").load(url2, function(){
-                $("#reviewform").hide();
-            });
-           });
+        $("#provider-detail").load(url2, function() {
+          $("#reviewform").hide();
+          });
+      });
 
-        });
-}
+    });
+  }
 
 });
 
-function sendInfo(id, phone){
-    console.log("in send info");
-    console.log(phone);
-    var url = "/sendinfo";
-    var data = $(this).serializeArray();
-    data.push({"name": "doctor_id", "value": id});
-    data.push({"name": "phone", "value": phone});
-    console.log(data);
-    $.post(url, data, function (result) {
-        console.log(result[Object][result][1]);
+function sendInfo(id, phone) {
+  console.log("in send info");
+  console.log(phone);
+  var url = "/sendinfo";
+  var data = $(this).serializeArray();
+  data.push({"name": "doctor_id", "value": id});
+  data.push({"name": "phone", "value": phone});
+  console.log(data);
+  $.post(url, data, function(result) {
+    console.log(result[Object][result][1]);
 
-    } );
+  });
 
 }
-
 
 //updates the pins displayed on map
-function mapSearch(){
+function mapSearch() {
 
     //remove current pinLayer from map
-    if (map.hasLayer(pinLayer)){
-        map.removeLayer(pinLayer);
-    }
+  if (map.hasLayer(pinLayer)) {
+    map.removeLayer(pinLayer);
+  }
 
     //add the pin layer to map
-    pinLayer.addTo(map);
-
-
+  pinLayer.addTo(map);
 
     //setFilter takes GeoJSON object, evaluates it, and returns true to show it and false to hide it
-    pinLayer.setFilter(function showDrs(feature){
+  pinLayer.setFilter(function showDrs(feature) {
         //if show insurance class is active, grab center and radius, and show pub insurance pins within radius
-        if (showIns.className === 'active' && showTrans.className === 'active' && showTherapy.className === '') {
-            return createLatLng(feature) &&
-            (feature.properties['ins'] === "yes") &&
-            (feature.properties['trans'] === "yes");
-        }
-        else if (showIns.className === '' && showTrans.className === 'active' && showTherapy.className === '') {
-            return createLatLng(feature) &&
-            (feature.properties['trans'] === "yes");
-        }
-        else if (showIns.className === 'active' && showTrans.className === '' && showTherapy.className === '') {
-            return createLatLng(feature) &&
-            (feature.properties['ins'] === "yes");
-        }
-        else if (showIns.className === '' && showTrans.className === '' && showTherapy.className === 'active') {
-            return createLatLng(feature) &&
-            (feature.properties['therapy'] === "yes");
-        }
-        else if (showIns.className === 'active' && showTrans.className === '' && showTherapy.className === 'active') {
-            return createLatLng(feature) &&
-            (feature.properties['therapy'] === "yes")&&
-            (feature.properties['ins'] === "yes");
-        }
-        else if (showIns.className === '' && showTrans.className === 'active' && showTherapy.className === 'active') {
-            return createLatLng(feature) &&
-            (feature.properties['therapy'] === "yes")&&
-            (feature.properties['trans'] === "yes");
-        }
-        else if (showIns.className === 'active' && showTrans.className === 'active' && showTherapy.className === 'active') {
-            return createLatLng(feature) &&
-            (feature.properties['therapy'] === "yes")&&
-            (feature.properties['trans'] === "yes")&&
-            (feature.properties['ins'] === "yes");
-        }
-        else {
-            return createLatLng(feature);
-        }
-    });
+    if (showIns.className === 'active' && showTrans.className === 'active' && showTherapy.className === '') {
+      return createLatLng(feature) &&
+      (feature.properties['ins'] === "yes") &&
+      (feature.properties['trans'] === "yes");
+    }else if (showIns.className === '' && showTrans.className === 'active' && showTherapy.className === '') {
+      return createLatLng(feature) &&
+      (feature.properties['trans'] === "yes");
+    }else if (showIns.className === 'active' && showTrans.className === '' && showTherapy.className === '') {
+      return createLatLng(feature) &&
+      (feature.properties['ins'] === "yes");
+    }else if (showIns.className === '' && showTrans.className === '' && showTherapy.className === 'active') {
+      return createLatLng(feature) &&
+      (feature.properties['therapy'] === "yes");
+    }else if (showIns.className === 'active' && showTrans.className === '' && showTherapy.className === 'active') {
+      return createLatLng(feature) &&
+      (feature.properties['therapy'] === "yes")&&
+      (feature.properties['ins'] === "yes");
+    }else if (showIns.className === '' && showTrans.className === 'active' && showTherapy.className === 'active') {
+      return createLatLng(feature) &&
+      (feature.properties['therapy'] === "yes")&&
+      (feature.properties['trans'] === "yes");
+    }else if (showIns.className === 'active' && showTrans.className === 'active' && showTherapy.className === 'active') {
+      return createLatLng(feature) &&
+      (feature.properties['therapy'] === "yes")&&
+      (feature.properties['trans'] === "yes")&&
+      (feature.properties['ins'] === "yes");
+    }else {
+      return createLatLng(feature);
+    }
+  });
 }
 
-
-function createLatLng(feature){
-    return(CENTER.distanceTo(L.latLng(
+function createLatLng(feature) {
+  return (CENTER.distanceTo(L.latLng(
         feature.geometry.coordinates[1],
         feature.geometry.coordinates[0])) < RADIUS);
 
 }
 
 //gets geocoded information for address with MapBox API
-function getGeocode(address){
+function getGeocode(address) {
     //converts address to a url form replacing spaces with +
-    address = address.replace(/ /g,"+");
+  address = address.replace(/ /g, "+");
     //the url for the get request
-    var mapBoxUrl = "http://api.tiles.mapbox.com/v4/geocode/mapbox.places/"+address+".json?access_token="+L.mapbox.accessToken;
+  var mapBoxUrl = "http://api.tiles.mapbox.com/v4/geocode/mapbox.places/"+address+".json?access_token="+L.mapbox.accessToken;
     // send this url with a get request to the mapbox geocoder api
-    $.get(mapBoxUrl, function (response) {
+  $.get(mapBoxUrl, function(response) {
         //  get an object back and pull out lat/lon
-        var lon = (response.features[0].center[0]);
-        var lat = (response.features[0].center[1]);
+    var lon = (response.features[0].center[0]);
+    var lat = (response.features[0].center[1]);
         // create a latLng object with the coordinates we got
-        CENTER = L.latLng(lat, lon);
+    CENTER = L.latLng(lat, lon);
         //get radius value from form and convert it to miles
-        RADIUS = $("#radiustext").val() * 1609.34;
+    RADIUS = $("#radiustext").val() * 1609.34;
         //call the mapSearch function
-        mapSearch();
+    mapSearch();
     //if we fail to get a response we'll print error, should do more here
-}).fail(function(error){
-    console.log('ERROR: ',error);
+  }).fail(function(error) {
+  console.log('ERROR: ', error);
 });
 
 }
+
 
